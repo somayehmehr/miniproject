5c1d1dff663b6b4a915d8edfc2046b06c3897cbd
سمیه مهرالحسنی <2992722588@tci.ir>
2024-11-25 12:23:48 +0330

    message is done

---
+++ message/tests.py
@@ -0,0 +1,24 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class MessagePagetest(SimpleTestCase):
+    def test_urlexist_at_correct_location(self):
+        response=self.client.get("/message/")
+        self.assertEqual(response.status_code,200)
+
+
+    def test_url_available_by_name(self):
+        response=self.client.get(reverse('message'))
+        self.assertEqual(response.status_code,200)
+
+
+    def test_template_name(self):
+        response=self.client.get(reverse('message'))
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_template_content(self):
+        response=self.client.get('/message/')
+        self.assertContains(response,' <h1>this is your message</h1>')
+
+
+# Create your tests here.
---
+++ message/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from .views import MessageView
+
+urlpatterns = [
+    path('',MessageView.as_view() ,name='message'),
+   
+]
